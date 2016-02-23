5c9763d9462450bd627f5326e5d433a40f740468
Bruno Lira <brunolyr4@gmail.com>
2016-02-23 07:44:15 -0300

    update

--- TextWhatcher/app/src/main/java/ifpb/edu/br/textwhatcher/ListaText.java
+++ TextWhatcher/app/src/main/java/ifpb/edu/br/textwhatcher/ListaText.java
@@ -4,24 +4,46 @@ import android.app.Activity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Bruno on 22/02/2016.
  */
 public class ListaText extends Activity implements TextWatcher {
 
+    //Adapter e lista
+
+    private 
+
+    EditText TextWhatcher = (EditText) findViewById(R.id.TextWhatcher);
+    TextWatcher.addTextChangedListener(this);
+
+    TextListView
+
     @Override
     public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
-        Log.i("charSequence","beforeTextChanged");
+        Log.i("TextWhatcher","beforeTextChanged: " + charSequence);
     }
 
     @Override
     public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
-        Log.i("charSequence","onTextChanged");
+        Log.i("TextWhatcher","onTextChanged: " + charSequence);
+
+        String nome = charSequence.toString();
+
+        nomes.add(nome);
+
+        adapter.notifyDataSetChanged();
+
+
     }
 
     @Override
     public void afterTextChanged(Editable editable) {
-        Log.i("editable","afterTextChanged");
+
+        Log.i("TextWhatcher","afterTextChanged: " + editable);
     }
 }
